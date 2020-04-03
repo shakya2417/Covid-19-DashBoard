1cef6a654c2182a1b728f77defeede12e50ad85c
shakya2417 <ramakantshakya@gmail.com>
2020-04-03 09:43:33 +0530

    make it better

--- mydash.py
+++ mydash.py
@@ -362,7 +362,7 @@ style_up={'text-align':'center','width':'300px','display':'inline-block'}
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#server=app.server
+server=app.server
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
 
